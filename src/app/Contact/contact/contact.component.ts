a9ef2ae4c766a41a9c574b4a6d6f5f5e9ee5443a
PeeyusSaini <sainipiyush801@gmail.com>
2024-07-18 13:21:25 +0530

    loader

--- src/app/Contact/contact/contact.component.ts
+++ src/app/Contact/contact/contact.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent implements OnInit,AfterViewInit {
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
     this.scrollToTop();
   }
   scrollToTop(): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+  
+  ngAfterViewInit(): void {
+    // After 3 seconds, hide the loader
+    setTimeout(() => {
+      const loader = this.elementRef.nativeElement.querySelector('#ftco-loader');
+      if (loader) {
+        loader.classList.remove('show');
+      }
+    }, 3000);
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,7 @@ import { NavigationEnd, Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent{
   title = 'MyPortfolioAng';
-  
-    constructor(private elementRef: ElementRef) { }
-  
-    ngAfterViewInit(): void {
-      // After 3 seconds, hide the loader
-      setTimeout(() => {
-        const loader = this.elementRef.nativeElement.querySelector('#ftco-loader');
-        if (loader) {
-          loader.classList.remove('show');
-        }
-      }, 3000); // 3000 milliseconds = 3 seconds
-    }
 
 }
